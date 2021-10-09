f99af4bb3a033957f21cd6c893eb18851a15bff7
Matt <dyermcd@gmail.com>
2021-10-09 16:31:00 -0500

    completed 11.5

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -1,26 +1,43 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloSpringController {
     // Responds to /hello?name=<name>
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model) {
+        String theGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
     // Responds to /hello/<name>
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloAgain(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name, Model model) {
+        String theGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
     @GetMapping("form")
     public String helloForm() {
         return "form";
     }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> controllerNames = new ArrayList<>();
+        controllerNames.add("LaunchCode");
+        controllerNames.add("Java");
+        controllerNames.add("JavaScript");
+        model.addAttribute("templateNames", controllerNames);
+        return "hello-list";
+    }
+
 }
